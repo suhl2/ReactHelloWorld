08cda9257abf32802fedeedc582ebad2b4767b41
Sarah <sarah.celiann@gmail.com>
2023-01-24 17:38:00 -0600

    profile finished

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Profile from "./profile";
 
 const App = (props) => {
-  return <div className="app">
-    <div className="profile">
-      <h1>Profile</h1>
-    </div>
-    <div className="dark-mode">
-      <h1>Dark Mode</h1>
-    </div>
-    <div className="tic-tac-toe">
-      <h1>Tic-Tac-Toe</h1>
-    </div>
-    <div className="todo">
-      <h1>Todo</h1>
-    </div>
-  </div>
-}
+        return <div className="app">
+          <Profile />
+          <div className="dark-mode">
+            <h1>Dark Mode</h1>
+          </div>
+          <div className="tic-tac-toe">
+            <h1>Tic-Tac-Toe</h1>
+          </div>
+          <div className="todo">
+            <h1>Todo</h1>
+          </div>
+        </div>
+      }
 
 ReactDOM.render(
   <App />,
---
+++ src/profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className="profile">
+  <h1>Profile</h1>
+  <img className='profile-pic' src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/39af738b-9c56-4e10-92a5-0803596e9df5/dcvmr5k-17bd749d-829d-43a9-ba69-fa4f62d95fe1.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzM5YWY3MzhiLTljNTYtNGUxMC05MmE1LTA4MDM1OTZlOWRmNVwvZGN2bXI1ay0xN2JkNzQ5ZC04MjlkLTQzYTktYmE2OS1mYTRmNjJkOTVmZTEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Oa7w0Q_wAo07UiuPAwJ5EEqWGidYvxtaV3a96NJih58'></img>
+  <h2 id='blue'>Steve Rogers</h2>
+  <p>AKA Captain America</p>
+  <h2>About Me</h2>
+  <p>I am a super solider recently unfrozen.  Life sure is weird in this century!</p>
+  <p>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere cursus egestas. Integer pharetra pharetra orci congue varius. Phasellus maximus velit eu metus sollicitudin suscipit. Sed lectus libero, vehicula eu nunc ac, mollis tristique enim. Maecenas vitae pretium enim. Ut laoreet est risus, non ultricies libero lobortis at. Etiam porttitor semper hendrerit. Pellentesque nulla eros, ornare vitae efficitur ut, ornare eu odio. In nulla arcu, ultricies vel est vel, finibus mollis magna. In id nunc non tortor tristique posuere. Integer non felis eros. Nunc et interdum lectus. Aliquam erat volutpat. Proin lorem magna, ultrices vel faucibus quis, pulvinar sed eros. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque hendrerit metus est, vitae ultrices mauris vestibulum ut.</p>
+</div>
+}
+
+export default Profile;
